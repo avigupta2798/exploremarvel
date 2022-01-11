a3faa8615503081359b4b12c72b058da46fca6e5
Avi Gupta <avibilasgupta@gmail.com>
2022-01-11 23:48:16 +0530

    started with models.py

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
 # Create your models here.
+
+class Comic(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    issueNumber = models.IntegerField()
+    comic_thumbnail_path = models.CharField(max_length=400)
+    comic_thumbnail_extension = models.CharField(max_length=5)
+
+class Characters(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    char_thumbnail_path = models.CharField(max_length=400)
+    char_thumbnail_extension = models.CharField(max_length=5)
+    comic = models.ManyToManyField(Comic, related_name='comics')
+
+class Events(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    char_thumbnail_path = models.CharField(max_length=400)
+    char_thumbnail_extension = models.CharField(max_length=5)
+    comic = models.ManyToManyField(Comic, related_name='comics')
+
+class Series(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=100)
+    char_thumbnail_path = models.CharField(max_length=400)
+    char_thumbnail_extension = models.CharField(max_length=5)
+    comic = models.ManyToManyField(Comic, related_name='comics')
